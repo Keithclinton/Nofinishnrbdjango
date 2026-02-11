105597087119fab0af56cc2d587893b330db2768
Keithclinton <keithnyaburi@gmail.com>
2026-02-11 22:01:59 +0300

    donations duplcate

--- donations/admin.py
+++ donations/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from .models import Donation
 
-admin.site.register(Donation)
-
 @admin.register(Donation)
 class DonationAdmin(admin.ModelAdmin):
-	list_display = ('user', 'amount', 'transaction_id', 'status', 'created_at')
-	search_fields = ('user__email', 'transaction_id')
-	list_filter = ('status',)
+    list_display = ('user', 'amount', 'transaction_id', 'status', 'created_at')
+    search_fields = ('user__email', 'transaction_id')
+    list_filter = ('status',)
